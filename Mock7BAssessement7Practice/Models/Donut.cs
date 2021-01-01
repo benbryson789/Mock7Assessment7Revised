5ea3c780740e7d75a9cbe7bed6490a75db36874e
benbryson789 <deborashelby@gmail.com>
2020-12-31 23:59:48 -0500

    initial commit

---
+++ Mock7BAssessement7Practice/Models/Donut.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mock7BAssessement7Practice.Models
+{   
+   
+    public class Donut
+    {
+        [Required]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public string PhotoURL { get; set; }
+        public string[] Extras { get; set; }
+    }
+}
